374a5ae556b6ccd55302ea05c5f8d22c871a0072
cardinal <0rdinal.inf0tec.electr0nics@gmail.com>
2017-07-23 04:57:44 +0900

    Fix bug of logger

--- tribunal-core/src/main/java/tribunal/tool/Logger.java
+++ tribunal-core/src/main/java/tribunal/tool/Logger.java
@@ -12,29 +12,29 @@ public class Logger {
         this.packageName = packageName;
     }
 
-    public void print(Object message){
+    public void print(String message){
         System.out.print(StringColor.RESET + message);
     }
 
-    public void println(Object message){
+    public void println(String message){
         System.out.println(StringColor.RESET + message);
     }
 
-    public void debug(Object message){
-        this.debug(message, StringColor.RESET);
+    public void debug(String message){
+        this.debug(message, "");
     }
 
-    public void debug(Object message, String color){
+    public void debug(String message, String color){
         SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String date = d.format(new Date());
         System.out.print(color + "[" + date + "] <" + packageName + "> " + message);
     }
 
-    public void debugln(Object message){
-        this.debugln(message, StringColor.RESET);
+    public void debugln(String message){
+        this.debugln(message, "");
     }
 
-    public void debugln(Object message, String color){
+    public void debugln(String message, String color){
         SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String date = d.format(new Date());
         System.out.println(color + "[" + date + "] <" + packageName + "> " + message);
@@ -44,17 +44,15 @@ public class Logger {
         this.print(StringColor.RESET);
     }
 
-    public void error(Object message, Exception e){
+    public void error(String message, Exception e){
         this.debugln(message, StringColor.RED);
         this.debug("", StringColor.RED);
         e.printStackTrace();
-        this.reset();
     }
 
     public void error(Exception e){
         this.debug("", StringColor.RED);
         e.printStackTrace();
-        this.reset();
     }
 
 }
