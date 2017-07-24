6a90c0608ae616b753c4c7a788f97d1b662f38a5
cardinal <0rdinal.inf0tec.electr0nics@gmail.com>
2017-07-24 15:55:33 +0900

    Fix color management

--- tribunal-core/src/main/java/tribunal/tool/Logger.java
+++ tribunal-core/src/main/java/tribunal/tool/Logger.java
@@ -8,49 +8,61 @@ public class Logger {
 
     String packageName;
 
-    public Logger(String packageName){
+    public Logger(String packageName) {
         this.packageName = packageName;
     }
 
-    public void print(String message){
+
+    public void print(String message) {
         System.out.print(StringColor.RESET + message);
     }
 
-    public void println(String message){
+    public void print(String message, String color) {
+        System.out.print(color + message);
+    }
+
+
+    public void println(String message) {
         System.out.println(StringColor.RESET + message);
     }
 
-    public void debug(String message){
-        this.debug(message, "");
+    public void println(String message, String color) {
+        System.out.println(color + message);
     }
 
-    public void debug(String message, String color){
+
+    public void debug(String message) {
+        this.debug(message, null);
+    }
+
+    public void debug(String message, String color) {
         SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String date = d.format(new Date());
         System.out.print(color + "[" + date + "] <" + packageName + "> " + message);
     }
 
-    public void debugln(String message){
-        this.debugln(message, "");
+
+    public void debugln(String message) {
+        this.debugln(message, null);
     }
 
-    public void debugln(String message, String color){
+    public void debugln(String message, String color) {
         SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String date = d.format(new Date());
         System.out.println(color + "[" + date + "] <" + packageName + "> " + message);
     }
 
-    public void reset(){
+    public void reset() {
         this.print(StringColor.RESET);
     }
 
-    public void error(String message, Exception e){
+    public void error(String message, Exception e) {
         this.debugln(message, StringColor.RED);
         this.debug("", StringColor.RED);
         e.printStackTrace();
     }
 
-    public void error(Exception e){
+    public void error(Exception e) {
         this.debug("", StringColor.RED);
         e.printStackTrace();
     }
